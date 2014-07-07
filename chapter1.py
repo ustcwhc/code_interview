9ed2c1e1e32071c875a9c06a2a13468a1c73aac3
ustcwhc <ustcwhc@gmail.com>
2014-07-07 10:58:57 +0800

    add c files

--- chapter1.py
+++ chapter1.py
@@ -2,5 +2,30 @@
 # filename: chapter1.py
 
 # PROBLEM 1.1
-# Implement an algorithm to determine if a string has
-# a unique characters
+# Implement an algorithm to determine if a string has a unique characters
+def is_unique_str(input_str):
+    appears = [False for i in range(0, 256)]
+    for c in input_str:
+        ascii_c = ord(c)
+        if appears[ascii_c]:
+            return False
+        else:
+            appears[ascii_c] = True
+
+    return True
+
+
+# PROBLEM 1.2
+# Reverse a string, pay attention to null string
+def reverse_string(input_str):
+    if not input_str:
+        return None
+    
+    chars = list(input_str)
+    return ''.join(reversed(chars))
+
+# MAIN FUNCTION
+if __name__ == "__main__":
+    print reverse_string('abcdefg')
+
+
---
+++ main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("\nHello World,\nWelcome to my first C program in Ubuntu Linux\n\n");
+    return(0);
+}
